f0eb6e2fde319dec4b90e9cb755b49e591add73e
iryna lysko <goiralysko@gmail.com>
2024-08-12 16:58:51 +0300

    make build

---
+++ index.d79f18f3.js
@@ -0,0 +1,2 @@
+var e=document.querySelectorAll("form > * > input"),r=!0,l=!1,a=void 0;try{for(var t,o=e[Symbol.iterator]();!(r=(t=o.next()).done);r=!0){var n,c=t.value,i=document.createElement("label"),m=(n=c.name).charAt(0).toUpperCase()+n.replace(/([a-z])([A-Z])/g,"$1 $2").toLowerCase().slice(1);i.className="field-label",i.htmlFor=c.id,i.innerText=m,c.placeholder=m,c.before(i)}}catch(e){l=!0,a=e}finally{try{r||null==o.return||o.return()}finally{if(l)throw a}}
+//# sourceMappingURL=index.d79f18f3.js.map
